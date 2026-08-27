adb2cab26b2dbc5ccfe2f564c4e7a98ba172b606
Владислав Рощупкин <vlad-roshchupkin@inbox.ru>
2026-08-27 12:39:31 +0700

    Создание JSON-сервера, добавление функции создания нового клиента

--- src/pages/Clients.tsx
+++ src/pages/Clients.tsx
@@ -1,7 +1,67 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Clients() {
+
+  type Clients = {
+    id: string;
+    firstName: string;
+    secondName: string;
+    date: Date;
+    height: number;
+    currentWeight: number;
+    desiredWeight: number;
+    goal: string;
+  }
+
+  const [clients, setClients] = useState<Clients[]>([]);
   const [isAddOpen, setIsAddOpen] = useState(false);
+
+  useEffect(() => {
+    const loadClients = async () => {
+      const response = await fetch('http://localhost:3000/clients');
+      const data = await response.json();
+
+      setClients(data);
+    };
+
+    loadClients();
+  }, [])
+
+  const handleCreateClient = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+
+    const payload = {
+      firstName: String(formData.get("firstName") ?? ""),
+      secondName: String(formData.get("secondName") ?? ""),
+      date: String(formData.get("date") ?? ""),
+      height: Number(formData.get("height") ?? ""),
+      currentWeight: Number(formData.get("currentWeight") ?? ""),
+      desiredWeight: Number(formData.get("desiredWeight") ?? ""),
+      goal: String(formData.get("goal") ?? ""),
+    };
+
+    const response = await fetch('http://localhost:3000/clients' , {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body : JSON.stringify(payload),
+    });
+
+    if (!response.ok) {
+      throw new Error('Не удалось создать клиента');
+    }
+
+    const createdClient = await response.json();
+
+    setClients((prevClients) => [...prevClients, createdClient]);
+    form.reset();
+    setIsAddOpen(false);
+  }
+
 
   return (
     <section>
@@ -13,10 +73,7 @@ function Clients() {
           <form
             className="max-h-[90vh] w-full max-w-lg overflow-y-auto rounded-2xl bg-[#242f3d] p-6 text-white shadow-2xl"
             onClick={(event) => event.stopPropagation()}
-            onSubmit={(event) => {
-              event.preventDefault();
-              setIsAddOpen(false);
-            }}
+            onSubmit={handleCreateClient}
           >
             <div className="mb-6 flex items-center justify-between gap-4">
               <h2 className="text-xl font-semibold">Новый клиент</h2>
@@ -148,26 +205,32 @@ function Clients() {
         />
       </div>
 
-      <div className="mt-5 flex items-center justify-between gap-2 rounded-xl border border-[#2f3b48] bg-[#242f3d] py-2 pl-3">
-        <div className="flex gap-2">
-          <h2>1</h2>
-          <h2>Имя</h2>
-        </div>
-        <div className="mr-2 flex gap-2">
-          <button
-            type="button"
-            className="cursor-pointer rounded-xl border border-[#2f3b48] bg-[#242f3d] px-6 py-2 text-[#a8b5c3] transition-all hover:bg-[#202b36] hover:text-white"
-          >
-            Пригласить клиента
-          </button>
-          <button
-            type="button"
-            className="cursor-pointer rounded-xl border border-[#2f3b48] bg-[#242f3d] px-4 py-1 text-[#a8b5c3] transition-all hover:bg-[#202b36] hover:text-white"
-          >
-            x
-          </button>
-        </div>
-      </div>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <div className="mt-5 flex items-center justify-between gap-2 rounded-xl border 
+            border-[#2f3b48] bg-[#242f3d] py-2 pl-3">
+              <span>{client.firstName}</span>
+              <div className="mr-2 flex gap-2">
+                <button
+                  type="button"
+                  className="cursor-pointer rounded-xl border border-[#2f3b48] bg-[#242f3d] px-6 py-2 
+                  text-[#a8b5c3] transition-all hover:bg-[#202b36] hover:text-white"
+                >
+                  Пригласить клиента
+                </button>
+                <button
+                  type="button"
+                  className="cursor-pointer rounded-xl border border-[#2f3b48] bg-[#242f3d] px-4 py-1 
+                  text-[#a8b5c3] transition-all hover:bg-[#202b36] hover:text-white"
+                >
+                  x
+                </button>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
